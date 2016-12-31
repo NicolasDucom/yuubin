cde999321a32e9851088fe9184eed232d9aec9de
Nicolas Ducom <nicolas.ducom@ext.adeoservices.com>
2016-12-31 19:19:01 +0100

    added call listing through socketio

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -1,4 +1,22 @@
 /**
  * Created by Nicolas on 22/12/16.
  */
-console.log("LOL");
+var socketUrl = "{{SOCKET_URL}}";
+
+var socket = io.connect(socketUrl);
+socket.on('news', function (data) {
+    console.log(data);
+    //socket.emit('other', {my: 'data'});
+});
+
+socket.on('httpEvent', function (data) {
+    var item = '<li> <div class="collapsible-header"><i class="material-icons">whatshot</i>' + data.http.method + '</div> ';
+    console.log(data.http.method);
+    if (['POST','PUT'].indexOf(data.http.method)!=-1) {
+        item += '<div class="collapsible-body"><p>' +
+            JSON.stringify(data.http.body) +
+            '</p></div>';
+    }
+    item += '</li>';
+    $('#httpCallList').prepend(item);
+});
--- index.js
+++ index.js
@@ -4,27 +4,51 @@
 var path = require('path');
 var absoluteify = require('absoluteify');
 var fs = require('fs');
-var cons = require('consolidate');
 var http = require('http');
 var socketio = require('socket.io');
-var server = null;
+const EventEmitter = require('events');
 var io = null;
+
 
 var express = require('express');
 var router = express.Router();
 
 module.exports = yuubin;
+module.exports.middleware = middleware;
 module.exports.router = router;
 
-var app;
 var params = {};
+
+const httpEventEmitter = new EventEmitter();
+
+function yuubin(params) {
+    console.log("yuubin setup");
+    this.params = params;
+    io = socketio(params.socketPort);
+    io.on('connection', function (socket) {
+        socket.emit('news', {hello: 'world'});
+        socket.on('my other event', function (data) {
+            console.log(data);
+        });
+    });
+}
+
+httpEventEmitter.on('httpEvent', function (req) {
+    io.sockets.emit('httpEvent', {
+            http: {
+                method: req.method,
+                url : req.url,
+                body : req.body
+            }
+        }
+    );
+});
 
 /* GET home page. */
 router.get('/', function (req, res, next) {
-    console.log(this.params);
     var resHTML = '';
     fs.createReadStream(path.join(__dirname, 'assets/index.html'))
-        .pipe(absoluteify(this.params.host + this.params.uri))
+        .pipe(absoluteify(this.params.host + ":" + this.params.hostPort + "/" + this.params.uri))
         .on('data', function (chunk) {
             resHTML += chunk.toString('utf8');
         })
@@ -37,15 +61,20 @@ router.get('/styles/:file', function (req, res, next) {
     return res.sendFile(path.join(__dirname, 'assets/styles/' + req.params.file));
 });
 
+router.get('/scripts/script.js', function (req, res, next) {
+    fs.readFile(path.join(__dirname, 'assets/scripts/script.js'), "utf-8", function (err, data) {
+        return res.send(data.replace("{{SOCKET_URL}}", this.params.host + ":" + this.params.socketPort));
+    });
+});
+
 router.get('/scripts/:file', function (req, res, next) {
     return res.sendFile(path.join(__dirname, 'assets/scripts/' + req.params.file));
 });
 
-function yuubin(params) {
-    this.params = params;
-    server = http.Server(params.app);
-    io = socketio(server);
-    return function middleware(req, res, next) {
-        next();
-    }
+function middleware(req, res, next) {
+    httpEventEmitter.emit("httpEvent", req);
+    next();
+
 }
+
+
